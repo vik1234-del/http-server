ebb6f0fd78c1aea410acb26170593db4b79d2a50
vik1234-del <victor.okolo@pau.edu.ng>
2024-01-20 17:26:00 +0100

    1st commit

---
+++ index.js
@@ -0,0 +1,29 @@
+//creating a simple server in nodejs
+
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+//create server
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/home") {
+    fs.readFile("./hello.txt", (err, data) => {
+      if (err) {
+        console.log(err);
+        res.write("Internal Server Error");
+        res.end();
+      } else {
+        res.write(data);
+        res.end();
+      }
+    });
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.write("Error, page not found");
+    res.end();
+  }
+});
+
+server.listen(8900, () => {
+  console.log("Server listening on port 8900");
+});
---
+++ steps.md
@@ -0,0 +1,21 @@
+Getting started with node.js
+
+Check if node is installed properly / node version
+$ node -v
+
+Initialize project
+$ npm init
+
+NPM - Node Package Manager
+Can also use Yarn or Pnpm --> should be installed separately
+
+Install dependencies
+$ npm install <package-name>
+
+To run a nodejs file
+$ node <file-name>.js
+
+Common core Modules
+
+Http
+Path
